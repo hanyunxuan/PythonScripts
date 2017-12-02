22db970fcc6f8ed84cd6cc6448f68699c5623689
hanyunxuan <408968382@qq.com>
2017-12-02 21:56:41 +0800

    no message

--- cplex.py
+++ cplex.py
@@ -1,13 +1,63 @@
- # from PyCPX import CPlexModel
- # from numpy import array, arange
- #
- # A = 2*arange(1,10).reshape( (3, 3) )
- # m = CPlexModel()
- #
- # X = m.new( (3, 3), vtype = int)
- # u = m.new( 3, vtype = int)
- # s = m.new(vtype = int)
- #
- # m.constrain(s <= A.T * X <= 10*s)
- # m.constrain(1 <= X.sum(axis = 1) <= u)
- #
+# from pulp import LpVariable, LpProblem, LpMinimize, GLPK, LpStatus, value
+#
+# x = LpVariable("x", 0, 3)
+# y = LpVariable("y", 0, 1)
+# prob = LpProblem("myProblem", LpMinimize)
+# prob += x + y <= 2
+# prob += -4*x + y
+# status = prob.solve()
+# # status = prob.solve(GLPK(msg = 0))
+# LpStatus[status]
+# value(x)
+
+# Import PuLP modeler functions
+
+
+# A new LP problem
+from pulp import LpProblem, LpMinimize, LpVariable, LpStatus, value
+
+prob = LpProblem("test1", LpMinimize)
+
+# Variables
+# 0 <= x <= 4
+x = LpVariable("x", 0, 4)
+# -1 <= y <= 1
+y = LpVariable("y", -1, 1)
+# 0 <= z
+z = LpVariable("z", 0)
+# Use None for +/- Infinity, i.e. z <= 0 -> LpVariable("z", None, 0)
+
+# Objective
+prob += x + 4*y + 9*z, "obj"
+# (the name at the end is facultative)
+
+# Constraints
+prob += x+y <= 5, "c1"
+prob += x+z >= 10, "c2"
+prob += -y+z == 7, "c3"
+# (the names at the end are facultative)
+
+# Write the problem as an LP file
+prob.writeLP("test1.lp")
+
+# Solve the problem using the default solver
+prob.solve()
+# Use prob.solve(GLPK()) instead to choose GLPK as the solver
+# Use GLPK(msg = 0) to suppress GLPK messages
+# If GLPK is not in your path and you lack the pulpGLPK module,
+# replace GLPK() with GLPK("/path/")
+# Where /path/ is the path to glpsol (excluding glpsol itself).
+# If you want to use CPLEX, use CPLEX() instead of GLPK().
+# If you want to use XPRESS, use XPRESS() instead of GLPK().
+# If you want to use COIN, use COIN() instead of GLPK(). In this last case,
+# two paths may be provided (one to clp, one to cbc).
+
+# Print the status of the solved LP
+print("Status:", LpStatus[prob.status])
+
+# Print the value of the variables at the optimum
+for v in prob.variables():
+	print(v.name, "=", v.varValue)
+
+# Print the value of the objective
+print("objective=", value(prob.objective))
